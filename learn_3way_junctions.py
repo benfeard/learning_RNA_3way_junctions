df9dd7cd6423e38eec7c5b6806dc3f1fb49290f1
benfeard <benfeard@gmail.com>
2020-11-11 21:39:50 -0500

    Update learn_3way_junctions.py

--- learn_3way_junctions.py
+++ learn_3way_junctions.py
@@ -6,4 +6,8 @@ Purpose: create a model that can predict the alignment of stems in an RNA 3-way 
 '''
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import sklearn as sk
+from sklearn.model_selection import train_test_split
+
+rna_junctions = pd.read_table('insert_file_name_here.txt')
