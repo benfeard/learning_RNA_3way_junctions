2ad1f91c3b90ec11be2df50dd5581598f8e85ae1
benfeard <benfeard@gmail.com>
2020-11-12 23:47:15 -0500

    Adding data and classifier

--- learn_3way_junctions.py
+++ learn_3way_junctions.py
@@ -10,4 +10,22 @@ import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.model_selection import train_test_split
 
-rna_junctions = pd.read_table('insert_file_name_here.txt')
+rna_3way = pd.read_csv('rna_junctions.csv')
+#print(rna_3way.head())
+
+# create a mapping from rna label to junction type
+lookup_junction_type = dict(zip(rna_3way.rna_label.unique(), rna_3way.junction_type.unique()))
+#print(lookup_junction_type)
+
+# split dataset
+X = rna_3way[['stem_a', 'junction_a', 'stem_b', 'junction_b', 'stem_c', 'junction_c']]
+y = rna_3way['rna_label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# naive Bayes classifer
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB().fit(X_train, y_train)
+
+classifier.score(X_test, y_test)
+predicted = classifier.predict(X_test)
