571cbebbd5d5c2aabd7f8f0f62f2536dfbf5d8e3
benfeard <benfeard@gmail.com>
2020-11-30 21:44:10 -0500

    Converting strings to vectors

--- learn_3way_junctions.py
+++ learn_3way_junctions.py
@@ -4,12 +4,15 @@ Created on Tue Nov 3, 2020
 
 Purpose: create a model that can predict the alignment of stems in an RNA 3-way junction
 '''
+import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 import sys
 
 rna_3way = pd.read_csv('rna_junctions.csv')
@@ -28,7 +31,59 @@ vector = vectorizer.transform(seq_text)
 # look at results
 print(vector.toarray())
 print(vectorizer.vocabulary_)
-sys.exit()
+
+# I need to make all my arrays the same length for classification
+def padarray64(my_array):
+    t = 64 - len(my_array)
+    return np.pad(my_array, pad_width=(0, t), mode='constant')
+
+
+# Ordinal encoding of RNA sequence data
+# "GUCA" --> [0.25, 0.5, 0.75, 1.0] 
+# although I hope this doesn't weight actual nucleotides
+# Let's make zero lenth junctions = 0
+label_encoder = LabelEncoder()
+label_encoder.fit(np.array(['G', 'U', 'C', 'A', 'Z']))
+
+
+def ordinal_encoder(my_seq):
+    my_seq = np.array(list(my_seq))
+    integer_encoded = label_encoder.transform(my_seq)
+    
+    float_encoded = integer_encoded.astype(float)
+    
+    float_encoded[float_encoded == 0] = 0.25 # A
+    float_encoded[float_encoded == 1] = 0.50 # C
+    float_encoded[float_encoded == 2] = 0.75 # G
+    float_encoded[float_encoded == 3] = 1.00 # U
+    float_encoded[float_encoded == 4] = 0.00 # Z (empty junction)
+    
+    return float_encoded
+
+
+# Optional one-hot encoding for future deep learning method
+# "ACGUZ" --> [1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1], [0,0,0,0]
+def one_hot_encoder(my_seq):
+    my_seq = np.array(list(my_seq))
+    integer_encoded = label_encoder.transform(my_seq)
+    
+    onehot_encoder = OneHotEncoder(sparse=False, dtype=int, categories=[range(5)])
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    onehot_encoded = np.delete(onehot_encoded, -1, 1)
+    
+    return onehot_encoded
+     
+# non label columns
+columns = [column for column in rna_3way][3:]
+
+for column in columns:
+    rna_3way[column] = rna_3way[column].apply(ordinal_encoder)
+    #rna_3way[column] = rna_3way[column].apply(one_hot_encoder)
+    rna_3way[column] = rna_3way[column].apply(padarray64)
+ 
+
+## new problem is that none of the sequences are the same length
 
 # split dataset
 # blue = A, green = B, red = C
